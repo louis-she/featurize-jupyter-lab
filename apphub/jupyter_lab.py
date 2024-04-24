a2997394327c6fb325273fc6a89d3bcc83e40652
chenglu <chenglu.she@gmail.com>
2024-04-24 13:59:27 +0800

    x

--- apphub/jupyter_lab.py
+++ apphub/jupyter_lab.py
@@ -63,7 +63,9 @@ Featurize 官方 JupyterLab 应用
         self.cfg.query = f"token={self.cfg.token}"
         super().installation(install_location, conda_env, work_dir)
         with self.conda_activate(conda_env):
-            self.execute_command(f"pip install {(Path(__file__).parent.parent / "workspace-0.1.0.tar.gz").as_posix()}")
+            self.execute_command(
+                f"pip install {(Path(__file__).parent.parent / 'workspace-0.1.0.tar.gz').as_posix()}"
+            )
         self.app_installed()
 
     def start(self):
